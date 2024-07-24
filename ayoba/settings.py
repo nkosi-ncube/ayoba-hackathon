98997992bf668ae7c92fd30f013ed2b06e684984
nkosi ncube <nkosi9976@gmail.com>
2024-07-24 16:42:17 +0200

    static files

--- ayoba/settings.py
+++ ayoba/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+      'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ayoba.urls'
@@ -118,7 +119,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+# Directory for static files collected during deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# URL for serving static files
+STATIC_URL = '/static/'
+
+# (Optional) Additional directories to search for static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# (Optional) To handle media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
