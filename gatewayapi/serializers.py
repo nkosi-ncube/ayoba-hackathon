af35e481b59c8ff189636d51dc2f37f140faf608
nkosi ncube <nkosi9976@gmail.com>
2024-07-26 16:05:50 +0200

    adding images to prdoucts

--- gatewayapi/serializers.py
+++ gatewayapi/serializers.py
@@ -16,7 +16,7 @@ class BusinessProfileSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ["id", "name", "price"]
+        fields = ["id", "name", "price",'image_url']
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
